c19ecd0f1f555b3b73c72d4c582fb7efa4f97461
Sukhiram Sahu <medcabit1@gmail.com>
2024-01-24 18:51:24 +0530

    login and logout

--- app/Livewire/Admin/LeftSidebar.php
+++ app/Livewire/Admin/LeftSidebar.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Livewire\Admin;
-
+use Session;
 use Livewire\Component;
 
 class LeftSidebar extends Component
@@ -9,5 +9,12 @@ class LeftSidebar extends Component
     public function render()
     {
         return view('livewire.admin.left-sidebar');
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        
+        return redirect()->to('/login')->with('success','Your Have Logout');
     }
 }
--- resources/views/livewire/admin/login-component.blade.php
+++ resources/views/livewire/admin/login-component.blade.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Adminoo | Log in</title>
-
+  <title>Admin || Log in</title>
+  @livewireStyles
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
@@ -19,59 +19,72 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+          <b>Medcab Admin</b>
   </div>
   <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <div class="toastrMsg">
+            @if(Session::has('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>{{ Session::get('success') }}</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
+         </div>
 
-      <form action="../../index3.html" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
 
-   
-      <!-- /.social-auth-links -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
 
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
-    </div>
-    <!-- /.login-card-body -->
-  </div>
+          <form wire:submit="login">
+
+              <div class="toastrMsg">
+                  @if(Session::has('error'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong>{{ Session::get('error') }}</strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+               </div>
+
+
+                <div class="input-group mb-3">
+                  <input type="email" class="form-control" placeholder="Enter the Your Email" autocomplete="off" wire:model="admin_id"  />
+                            @error("admin_id")  <p class="pt-2 px-1 text-danger">{{$message }}</p> 
+                            @enderror
+                      <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                      </div>
+                  </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Enter Your Password" autocomplete="off" wire:model="admin_pass"  />
+                            @error("form.admin_pass")  <p class="pt-2 px-1 text-danger">{{ 
+                            str_replace('form.admin_pass', 'admin_pass', $message) }}</p>  @enderror
+                          <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-lock"></span>
+                    </div>
+                  </div>
+                </div>
+                  <div class="row float-right">
+                    <!-- /.col -->
+                    <div class="col-4">
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+          </form>
+
+          <!-- /.social-auth-links -->
+        </div>
+        <!-- /.login-card-body -->
+      </div>
 </div>
 <!-- /.login-box -->
 
@@ -80,11 +93,11 @@
  <!-- AdminLTE App -->
  
 
-
 <script src="{{url('backend_assets/js/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
 <script src="{{url('backend_assets/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{url('backend_assets/js/adminlte.min.js')}}"></script>
+@livewireScripts
 </body>
 </html>
