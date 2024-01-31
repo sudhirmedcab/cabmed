8eedc4710b374fa9a4258dc98194c2333bc5433a
sudhirmedcab <157484139+sudhirmedcab@users.noreply.github.com>
2024-01-31 15:47:02 +0530

    Driver module filter and design update
    Navigation filter tab design for driver module
    List all drivers
    filter list Active drivers
    filter on duty users
    filter off duty users
    filter Under Verification by self
    filter Under Verification by partner

--- app/Livewire/Admin/DriverComponent.php
+++ app/Livewire/Admin/DriverComponent.php
@@ -8,7 +8,16 @@ use Livewire\WithPagination;
 
 class DriverComponent extends Component
 {
-    public $employees0, $id, $name, $email, $position, $employee_id;
+    public $employees0,$selectedDate,$filterConditionl, 
+    $selectedFromDate,$selectedToDate, $fromDate=null, 
+    $toDate=null,$fromdate, $todate, $id, $name, $email,
+    $position, $employee_id, $driver_status=null, $driver_duty_status=null,
+    $driverUnderVerBySelf=null,
+    $driverUnderVerByPartner=null,
+    $activeTab,
+    $division,
+    $walletBalanceFilter,
+    $driverVerificationStatus = null;
     public $isOpen = 0;
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
@@ -16,13 +25,179 @@ class DriverComponent extends Component
     public $search = '';
     public $sortBy = 'name';
     public $sortDirection = 'asc';
+     public $age = '30';
+    public $address = 'Lucknow';
+    public function mount()
+    {
+        $driverVerificationStatus = $this->driverVerificationStatus ? $this->driverVerificationStatus : null;
+    }
 
+    public function activeDriver($value){
+    //    dd('ddfdfdf',$value);
+    if($value=='Active'){
+        // $this->activeDriver = 1;
+        $this->activeTab=$value;
+        $this->driver_status=1;
+    }
+    if($value=='Onduty'){
+        // $this->activeDriver = 1;
+    $this->driver_status=null;
+    $this->driver_duty_status='ON';
+
+    $this->activeTab=$value;
+    }
+    if($value=='Offduty'){
+    // $this->activeDriver = 1;
+    $this->driver_status=null;
+    $this->driver_duty_status='OFF';
+    $this->activeTab=$value;
+    }
+
+    }
+
+    public function filterCondition($value){
+             if($value=='Active'){
+                // $this->activeDriver = 1;
+                $this->driver_duty_status=null;
+                $this->driverUnderVerBySelf=null;
+                $this->driverUnderVerByPartner=null;
+                $this->activeTab=$value;
+                $this->driver_status=1;
+            }
+        
+            if($value=='Onduty'){
+                // $this->activeDriver = 1;
+                $this->driver_status=null;
+                $this->driverUnderVerBySelf=null;
+                $this->driverUnderVerByPartner=null;
+                $this->driver_duty_status='ON';
+                $this->activeTab=$value;
+            }
+            if($value=='Offduty'){
+                // $this->activeDriver = 1;
+                $this->driver_status=null;
+                $this->driverUnderVerBySelf=null;
+                $this->driverUnderVerByPartner=null;
+                $this->driver_duty_status='OFF';
+                $this->activeTab=$value;
+            }
+            if($value=='UnderVerificationBySelf'){
+                $this->driver_status=null;
+                $this->driver_duty_status=null;
+                $this->driverUnderVerByPartner=null;
+                $this->driverUnderVerBySelf='yes';
+                $this->activeTab=$value;
+            }
+            if($value=='UnderVerificationByPartner'){
+                $this->driver_status=null;
+                $this->driver_duty_status=null;
+                $this->driverUnderVerBySelf=null;
+                $this->activeTab=$value;
+                $this->driverUnderVerByPartner='yes';
+            }
+            if($value=='walletBalance'){
+                // $this->activeDriver = 1;
+                $this->driver_duty_status=null;
+                $this->driverUnderVerBySelf=null;
+                $this->driverUnderVerByPartner=null;
+                $this->activeTab=$value;
+                $this->driver_status=null;
+            }
+            if($value=='division'){
+                // $this->activeDriver = 1;
+                $this->driver_duty_status=null;
+                $this->driverUnderVerBySelf=null;
+                $this->driverUnderVerByPartner=null;
+                $this->activeTab=$value;
+                $this->driver_status=null;
+            }
+    
+        }
  
     public function render()
     {
-
+// dd($this->activeDriver);
+$division = $this->division ? $this->division : null;
+if($this->activeTab=='division'){
+    return view('livewire.hello-world')->layout('livewire.admin.layouts.base');
+    }
         $currentTimestamps = Carbon::now();
         $firstDayOfMonths = Carbon::now()->startOfMonth(); 
+    //    dd($this->selectedDate);
+        // ...
+        // dd($this->driver_status);
+        // dd($this->driverVerificationStatus);
+        $driver_status = $this->driver_status ? $this->driver_status : null;
+        $driver_duty_status = $this->driver_duty_status ? $this->driver_duty_status : null;
+        $driverUnderVerByPartner = $this->driverUnderVerByPartner ? $this->driverUnderVerByPartner : null;
+        $driverUnderVerBySelf = $this->driverUnderVerBySelf ? $this->driverUnderVerBySelf : null;
+        $driverVerificationStatus = $this->driverVerificationStatus ? $this->driverVerificationStatus : null;
+        $walletBalanceFilter = $this->walletBalanceFilter ? $this->walletBalanceFilter : null;
+
+        // dd($driver_status,$driver_duty_status);
+        $fromDate = $this->selectedFromDate ? Carbon::createFromFormat('Y-m-d', $this->selectedFromDate)->startOfDay() : null;
+        $toDate = $this->selectedToDate ? Carbon::createFromFormat('Y-m-d', $this->selectedToDate)->endOfDay() : null;
+        if($this->selectedFromDate && $this->selectedToDate){
+            $this->selectedDate = null;     
+        }
+        if($this->selectedDate){
+            $this->selectedFromDate ='';
+            $this->selectedToDate ='';
+        }
+       
+        switch ($this->driverVerificationStatus) {
+            case 'UnderVerification':
+                $fromDate = null;
+                $toDate = null;
+                break;
+            case 'UnderVerificationBySelf':
+                $this->driver_status=null;
+                $this->driver_duty_status=null;
+                $this->driverUnderVerByPartner=null;
+                $this->driverUnderVerBySelf='yes';
+                break;
+            case 'UnderVerificationByPartner':
+                $this->driver_status=null;
+                $this->driver_duty_status=null;
+                $this->driverUnderVerBySelf=null;
+                $this->driverUnderVerByPartner='yes';
+                break;
+            default:
+                break;
+        }
+        switch ($this->selectedDate) {
+            case 'all':
+                $fromDate = null;
+                $toDate = null;
+                break;
+            case 'today':
+                $fromDate = Carbon::today();
+                $toDate = Carbon::today()->endOfDay();
+                break;
+            case 'yesterday':
+                $fromDate = Carbon::yesterday();
+                $toDate = Carbon::yesterday()->endOfDay();
+                break;
+            case 'thisWeek':
+                $fromDate = Carbon::now()->subDays(7)->startOfDay();
+                $toDate = Carbon::now();
+                break;
+            case 'thisMonth':
+                $fromDate = Carbon::now()->startOfMonth();
+                $toDate = Carbon::now()->endOfMonth();
+                break;
+            default:
+                $fromDate = $fromDate;
+                $toDate = $toDate;
+                break;
+        }
+       
+        // ...
+
+
+
+
+
         $drivers = DB::table('driver')
         ->leftJoin('driver_details', 'driver.driver_id', '=', 'driver_details.driver_details_driver_id')
         ->leftJoin('vehicle', 'driver.driver_assigned_vehicle_id', '=', 'vehicle.vehicle_id')
@@ -31,7 +206,50 @@ class DriverComponent extends Component
         ->leftJoin('state', 'state.state_id', '=', 'city.city_state')
         ->leftJoin('ambulance_category', 'ambulance_category.ambulance_category_type', '=', 'vehicle.vehicle_category_type')
         ->leftJoin('remark_data', 'driver.driver_id', '=', 'remark_data.remark_driver_id')
-        // ->whereBetween(('driver.created_at'), [$firstDayOfMonths, $currentTimestamps])
+        ->when($fromDate && $toDate, function ($query) use ($fromDate, $toDate) {
+            return $query->whereBetween('driver.created_at', [$fromDate, $toDate]);
+        })
+        ->when($driver_status !== null, function ($query) use ($driver_status) {
+            return $query->where('driver.driver_status', $driver_status);
+        })
+        ->when($driver_duty_status !== null, function ($query) use ($driver_duty_status) {
+            return $query->where('driver.driver_duty_status', $driver_duty_status);
+        })
+        ->when($walletBalanceFilter !==null && $walletBalanceFilter == 'zeroBalance', function ($query) use ($walletBalanceFilter){
+            return $query->where('driver.driver_wallet_amount',0);
+        })
+        ->when($walletBalanceFilter !==null && $walletBalanceFilter == 'negativeBalance', function ($query) use ($walletBalanceFilter){
+            return $query->where('driver.driver_wallet_amount','<',0);
+        })
+        ->when($walletBalanceFilter !==null && $walletBalanceFilter == 'positiveBalance', function ($query) use ($walletBalanceFilter){
+            return $query->where('driver.driver_wallet_amount','>',0);
+        })
+
+        // ->when($driverUnderVerByPartner !== null, function ($query) use ($driverUnderVerByPartner) {
+        //     return $query
+        //         ->where('driver.driver_status', 4)
+        //         ->where('driver_created_by', 1);
+        // })
+        // ->when($driverUnderVerBySelf !== null, function ($query) use ($driverUnderVerBySelf) {
+        //     return $query
+        //         ->where('driver.driver_status', 4)
+        //         ->where('driver_created_by', 0);
+        // }) 
+        ->when($driverVerificationStatus == 'UnderVerification', function ($query) use ($driverVerificationStatus) {
+            return $query
+                ->where('driver.driver_status', 4)
+                ->where('driver_created_by', 0);
+        })
+        ->when($driverVerificationStatus == 'UnderVerificationBySelf', function ($query) use ($driverVerificationStatus) {
+            return $query
+                ->where('driver.driver_status', 4)
+                ->where('driver_created_by', 0);
+        })
+        ->when($driverVerificationStatus == 'UnderVerificationByPartner', function ($query) use ($driverVerificationStatus) {
+            return $query
+                ->where('driver.driver_status', 4)
+                ->where('driver_created_by', 1);
+        })
         ->where('driver.driver_name', 'like', '%' . $this->search . '%')
         ->select('driver_details.*','ambulance_category.*','remark_data.*', 'vehicle.*', 'city.*', 'partner.*', 'state.*', 'driver.driver_id', 'driver.driver_name', 'driver.driver_last_name', 'driver.driver_mobile','driver.driver_wallet_amount','driver.driver_duty_status','driver.join_bonus_status', 'driver.driver_on_booking_status','driver.driver_created_by', 'driver.created_at', 'driver.driver_verify_date','driver.driver_status')
         ->orderBy('driver.driver_id', 'desc')
--- resources/views/livewire/admin/driver-component.blade.php
+++ resources/views/livewire/admin/driver-component.blade.php
@@ -1,4 +1,4 @@
-<div class="content" wire:init="livewire.admin.DriverComponent">
+<div class="content">
     <div class="container-fluid">
         @if ($isOpen)
             @include('livewire.employee-form')
@@ -8,7 +8,6 @@
         @elseif (session()->has('message') && session()->has('type') == 'store')
             <div class="alert alert-success">{{ session('message') }}</div>
         @endif
-         {{ $search }}
          <div class="card text-center">
           <div class="card-header pt-1 pb-3">
             <ul class="nav nav-tabs custom__nav__tab  card-header-tabs">
@@ -19,7 +18,9 @@
                 <a class="nav-link fs-1" href="/manage-driver" wire:navigate class="nav-link" >Add</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link fs-1" href="#">Devision</a>
+                <a class="custom__nav__btn nav-link fs-1 {{$this->activeTab == 'division' ? 'btn-primary':''}}" wire:click="filterCondition('division')">
+                Division
+              </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link fs-1" href="#">District</a>
@@ -28,23 +29,35 @@
                 <a class="nav-link fs-1" href="#">Under Reg.</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link fs-1" href="#">Under FRC</a>
-              </li><li class="nav-item">
-                <a class="nav-link fs-1" href="#">Active</a>
+              <a class="custom__nav__btn nav-link fs-1 {{$this->activeTab == 'walletBalance' ? 'btn-primary':''}}" wire:click="filterCondition('walletBalance')">
+               Wallet
+              </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link fs-1" href="#">On Duty</a>
+                <a class="custom__nav__btn nav-link fs-1 {{$this->activeTab == 'Active' ? 'btn-primary':''}}" wire:click="filterCondition('Active')">
+                  Active
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link fs-1" href="#">Off Duty</a>
+              <a class="custom__nav__btn nav-link fs-1 {{$this->activeTab == 'Onduty' ? 'btn-primary':''}}" wire:click="filterCondition('Onduty')">
+                  On Duty
+              </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link fs-1" href="#">Verify By driver</a>
+                <a class="custom__nav__btn nav-link fs-1 {{$this->activeTab == 'Offduty' ? 'btn-primary':''}}" wire:click="filterCondition('Offduty')">
+                Off Duty
+                </a>
               </li>
-
               <li class="nav-item">
-                <a class="nav-link fs-1" href="#">Verify By Partner</a>
+              <a class="custom__nav__btn nav-link fs-1 {{$this->activeTab == 'UnderVerificationBySelf' ? 'btn-primary':''}}" wire:click="filterCondition('UnderVerificationBySelf')">
+              Under Verification
+              </a>
               </li>
+<!-- 
+              <li class="nav-item">
+              <a class="custom__nav__btn nav-link fs-1 {{$this->activeTab == 'UnderVerificationByPartner' ? 'btn-primary':''}}" wire:click="filterCondition('UnderVerificationByPartner')">
+              Under Verification (ByPartner)
+              </a>              </li> -->
             </ul>
           </div>
         
@@ -52,52 +65,161 @@
             
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">
-                <button wire:click="create()" class="mb-2 btn btn-sm btn-primary"><i class="fa fa-plus"></i></button>
-                </h3>
-
-                <div class="card-tools">
-                  <div class="input-group input-group-sm" style="width: 150px;">
-                    <input type="search" wire:model.live.debounce.150ms="search" class="form-control float-right" placeholder="Search">
-
-                    <div class="input-group-append">
-                      <button type="submit" class="btn btn-default">
-                        <i class="fas fa-search"></i>
-                      </button>
+               
+                <div class="row">
+                    <div class="col __col-{{$this->activeTab == 'UnderVerificationBySelf' || $this->activeTab == 'walletBalance' ? 2:3}}">
+                      <div class="form-group">
+                          <label class="custom__label" for="fromDate">From </label>
+                          <input wire:model.live="selectedFromDate" type="date" class="custom__input__field rounded-0 form-control form-control-sm" id="fromDate" placeholder="Enter from date">
+                      </div>
                     </div>
-                  </div>
+                    <div class="col __col-{{$this->activeTab == 'UnderVerificationBySelf' || $this->activeTab == 'walletBalance' ? 2:3}}">
+                      <div class="form-group">
+                        <label class="custom__label" for="toDate">To</label>
+                        <input wire:model.live="selectedToDate" type="date" class="custom__input__field rounded-0 form-control form-control-sm" id="toDate" placeholder="Enter to date">
+                      </div>
+                    </div>
+                    <div class="col -{{$this->activeTab == 'UnderVerificationBySelf' || $this->activeTab == 'walletBalance' ? 2:3}}">
+                        <div class="form-group">
+                          <label class="custom__label">Select</label>
+                          <select wire:model.live.debounce.150ms="selectedDate" wire:mode.live="selectedDate" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="exampleSelectRounded0">
+                          <option selected value="all">All</option>
+                          <option value="today">Today</option>
+                          <option value="yesterday">Yesterday</option>
+                          <option value="thisWeek">This Week</option>
+                          <option value="thisMonth">This Month</option>
+                        </select>
+                        </div>
+                    </div>
+                    @if($this->activeTab == 'UnderVerificationBySelf')
+                    <div class="col __col-3">
+                        <div class="form-group">
+                          <label class="custom__label">Driver By Status</label>
+                          <select wire:model.live.debounce.150ms="driverVerificationStatus" wire:mode.live="driverVerificationStatus" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                          <option selected value="UnderVerification">Under Verification(All)</option>
+                          <option  value="UnderVerificationBySelf">Under Verification(By Self)</option>
+                          <option  value="UnderVerificationByPartner">Under Verification(By Partner)</option>
+                         
+                        </select>
+                        </div>
+                    </div>
+                    @endif
+
+                    @if($this->activeTab == 'walletBalance')
+                    <div class="col __col-3">
+                        <div class="form-group">
+                          <label class="custom__label">Wallet Balance</label>
+                          <select wire:model.live.debounce.150ms="walletBalanceFilter" wire:mode.live="walletBalanceFilter" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                          <option  value="positiveBalance">Positive Balance</option>
+                          <option  value="zeroBalance">Zero Balance</option>
+                          <option  value="negativeBalance">Negative Balance</option>
+                        </select>
+                        </div>
+                    </div>
+                    @endif
+
+                    <div class="col __col-3">
+                      <div class="form-group">
+                          <label class="custom__label" for="toDate">Search</label>
+                          <input type="search" wire:model.live.debounce.150ms="search" class="custom__input__field form-control rounded-0 form-control-sm float-right" placeholder="Search">
+                      </div>
+                    </div>
                 </div>
+                
+              </div>
+              <div class="row d-none card-header">
+                <div class="col-3">
+                          <div class="form-group">
+                            <label class="custom__label">Wallet Balance</label>
+                            <select wire:model.live.debounce.150ms="walletBalanceFilter" wire:mode.live="walletBalanceFilter" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                            <option  value="positiveBalance">Positive Balance</option>
+                            <option  value="zeroBalance">Zero Balance</option>
+                            <option  value="negativeBalance">Negative Balance</option>
+                          </select>
+                          </div>
+                      </div>
+                      <div class="col-3">
+                          <div class="form-group">
+                            <label class="custom__label">Wallet Balance</label>
+                            <select wire:model.live.debounce.150ms="walletBalanceFilter" wire:mode.live="walletBalanceFilter" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                            <option  value="positiveBalance">Positive Balance</option>
+                            <option  value="zeroBalance">Zero Balance</option>
+                            <option  value="negativeBalance">Negative Balance</option>
+                          </select>
+                          </div>
+                      </div>
+                      <div class="col-3">
+                          <div class="form-group">
+                            <label class="custom__label">Wallet Balance</label>
+                            <select wire:model.live.debounce.150ms="walletBalanceFilter" wire:mode.live="walletBalanceFilter" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                            <option  value="positiveBalance">Positive Balance</option>
+                            <option  value="zeroBalance">Zero Balance</option>
+                            <option  value="negativeBalance">Negative Balance</option>
+                          </select>
+                          </div>
+                      </div>
+                      <div class="col-3">
+                          <div class="form-group">
+                            <label class="custom__label">Wallet Balance</label>
+                            <select  class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                            <option  value="positiveBalance">Positive Balance</option>
+                            <option  value="zeroBalance">Zero Balance</option>
+                            <option  value="negativeBalance">Negative Balance</option>
+                          </select>
+                          </div>
+                      </div>
               </div>
             <!-- <table class="table table-bordered table-sm table-responsive-sm mt-2"> -->
                 <div class="card-body p-0">
                 <table class="table custom__table table-bordered table-sm">
                 <tr>
-                    <th>Driver Id</th>
+                    <th>Sr.</th>
+                    <th>Id</th>
                     <th>Name</th>
-                    <th>Created Date</th>
+                    <th>Mobile</th>
+                    <th>Wallet</th>
+                    <th>Created At</th>
                     <th>Created By</th>
-                    <th>Driver Status</th>
+                    <th>Bonus</th>
+                    <th>Duty</th>
+                    <th>Booking</th>
                     <!-- <th>Remark Details</th> -->
                     <th>City</th>
                     <th>RC No.</th>
                     <!-- <th>DL No.</th> -->
                     <th>Action</th>
                 </tr>
+@php
+$srno = 1
+@endphp
                 @foreach ($drivers as $driver)
                     <tr>
+                        <td>{{ $srno }}</td>
                         <td>{{ $driver->driver_id }}</td>
                         <td>{{ $driver->driver_name.' '.$driver->driver_last_name }}</td>
+                        <td>{{ $driver->driver_mobile }}</td>
+                        <td>{{ $driver->driver_wallet_amount }}</td>
+
                         <td>{{ $driver->created_at }}</td>
                         <td>
                         @if($driver->driver_created_by=='0')
-												 Self Created
+												 Self
 												 @else
-												 Partner Created
+												 Partner
                         @endif
                         </td>
-                        <td>{{ $driver->driver_mobile }}</td>
-                        <td>{{ $driver->driver_mobile }}</td>
-                        <td>{{ $driver->driver_mobile }}</td>
+                        <td>
+                        {{ $driver->join_bonus_status ==1 ? 'Yes' : 'No' }}
+
+                        </td>
+                        <td>
+                          {{ $driver->driver_duty_status }}
+                          <!-- Booking Status: {{ $driver->driver_on_booking_status == 0 ? 'Free' : 'In Booking' }} -->
+                        </td>
+                        
+                        <td>{{ $driver->driver_on_booking_status == 0 ? 'Free' : 'Ongoing' }}</td>
+                        <td>{{ $driver->city_name }}</td>
+                        <td>{{ $driver->vehicle_rc_number }}</td>
                         <!-- <td>{{ $driver->driver_mobile }}</td>
                         <td>{{ $driver->driver_mobile }}</td>
                         <td>{{ $driver->driver_mobile }}</td>
@@ -108,8 +230,14 @@
                                 wire:click="delete({{ $driver->driver_id }})" class="pt-0 pl-2 pr-2 pb-1 btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
                         </td>
                     </tr>
+                    @php
+                    $srno++
+                    @endphp
                 @endforeach
+
             </table>
+            <div style="text-align:center;" wire:loading wire:target="selectedDate" wire:key="selectedDate"><i class="fa fa-spinner fa-spin mt-2 ml-2"></i>loading</div>
+
             </div>
             </div>
           
