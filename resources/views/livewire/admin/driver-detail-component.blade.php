b2271ff036bcbddd6ef4a3f97d4a4185d31bbc36
Uttam Shrestha <shresthauttam66@gmail.com>
2024-02-05 10:08:44 +0530

    driver detail page added

--- resources/views/livewire/admin/driver-detail-component.blade.php
+++ resources/views/livewire/admin/driver-detail-component.blade.php
@@ -1,3 +1,192 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div class="content">
+    <div class="container-fluid">
+        <div class="card my-2  text-center">
+            <div class="card-header custom__driver__filter">
+                <ul class="nav nav-tabs custom__nav__tab  card-header-tabs">
+                    <li class="nav-item  active">
+                        <a class="nav-link fs-1" href="">Detail</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-1" href="">Enquiry</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-1" href="">New Booking</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-1" href="">Ongoing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-1" href="">Invoice</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-1" href="">Complete</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-1" href="">Cancel</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-1" href="">Transaction</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-1" href="">Map View</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
+
+
+        <div class="custom__driver__detail__section card px-3">
+            <table class="table m-0 table-sm custom__table table-bordered mt-3 mb-2">
+                <th>Type - Self</th>
+                <th>Verification</th>
+                <th>Unverify</th>
+            </table>
+            <div class="row p-0">
+                <div class="col-12 col-md-6 px-0">
+                    <div class=" pl-3 py-2">
+                        <table class="table m-0 custom__table table-bordered">
+                            <tr>
+                                <td>Name</td>
+                                <td>Uttam</td>
+                            </tr>
+                            <tr>
+                                <td>Mobile Number</td>
+                                <td>9125106355 (Self)</td>
+                            </tr>
+                            <tr>
+                                <td>Profile</td>
+                                <td><a href="">View Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>DOB</td>
+                                <td>26/09/20002</td>
+                            </tr>
+                            <tr>
+                                <td>Gender</td>
+                                <td>Male</td>
+                            </tr>
+                            <tr>
+                                <td>City, State</td>
+                                <td>Lucknow, Up</td>
+                            </tr>
+                            <tr>
+                                <td>Aadhaar Front</td>
+                                <td><a href="">View Aadhaar Front Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>Aadhaar Back</td>
+                                <td><a href="">View Aadhaar Back Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>Aadhaar Number</td>
+                                <td>984984651651984</td>
+                            </tr>
+                            <tr>
+                                <td>PAN Image</td>
+                                <td><a href="">View PAN Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>PAN Number</td>
+                                <td>AFDSAA65435</td>
+                            </tr>
+                            <tr>
+                                <td>Police Verification Image</td>
+                                <td><a href="">View Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>Police Verification Expiry</td>
+                                <td>2/3/2028</td>
+                            </tr>
+                            <tr>
+                                <td>DL Front</td>
+                                <td>View DL Front Image</td>
+                            </tr>
+                            <tr>
+                                <td>DL Back</td>
+                                <td><a href="">View DL Back Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>DL Number</td>
+                                <td>984651984951</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 px-0">
+                    <div class=" pr-3 py-2">
+                        <table class="table m-0 custom__table table-bordered">
+                            <tr>
+                                <td>DL Expiry</td>
+                                <td>2/3/2029</td>
+                            </tr>
+                            <tr>
+                                <td>Ambulane Front</td>
+                                <td><a href="">View Ambulane Front Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>Ambulance Back</td>
+                                <td><a href="">View Ambulance Back Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>Category</td>
+                                <td>Road Ambulance</td>
+                            </tr>
+                            <tr>
+                                <td>Facilities</td>
+                                <td>BLS</td>
+                            </tr>
+                            <tr>
+                                <td>RC Image</td>
+                                <td><a href="">View RC Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>RC Number</td>
+                                <td>1685498615</td>
+                            </tr>
+                            <tr>
+                                <td>RC Expiry</td>
+                                <td>3/6/2030</td>
+                            </tr>
+                            <tr>
+                                <td>Fitness Image</td>
+                                <td><a href="">View Fitness Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>Fitness Expiry</td>
+                                <td>3/6/2030</td>
+                            </tr>
+                            <tr>
+                                <td>Insurance Image</td>
+                                <td><a href="">View Insurance Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>Insurance Expiry</td>
+                                <td>9125106355</td>
+                            </tr>
+                            <tr>
+                                <td>Insurance Holder Name</td>
+                                <td>Sukhiram</td>
+                            </tr>
+                            <tr>
+                                <td>Pollution Image</td>
+                                <td><a href="">View Pollution Image</a></td>
+                            </tr>
+                            <tr>
+                                <td>Pollution Expiry</td>
+                                <td>9125106355</td>
+                            </tr>
+                            <tr>
+                                <td>Partner Name & Number</td>
+                                <td>Partner, 123941923</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
 </div>
