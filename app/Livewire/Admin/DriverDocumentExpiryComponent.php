7fa5c6d21c38db82fd810e6165fbb5e4132c102c
sudhirmedcab <157484139+sudhirmedcab@users.noreply.github.com>
2024-02-14 13:10:21 +0530

    driver document expiry component
    driver document expiry component
    search filter, date filter

---
+++ app/Livewire/Admin/DriverDocumentExpiryComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class DriverDocumentExpiryComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.driver-document-expiry-component');
+    }
+}
---
+++ resources/views/livewire/admin/driver-document-expiry-component.blade.php
@@ -0,0 +1,233 @@
+<div class="content">
+    <div class="container-fluid">
+        @if ($isOpen)
+        @include('livewire.employee-form')
+        @endif
+        @if (session()->has('message') && session()->has('type') == 'delete')
+        <div class="alert alert-danger">{{ session('message') }}</div>
+        @elseif (session()->has('message') && session()->has('type') == 'store')
+        <div class="alert alert-success">{{ session('message') }}</div>
+        @endif
+        @include('livewire.admin.driver-nav-component')
+
+        <div class="card ">
+            <div class="card-header custom__filter__select ">
+
+                <div class="row">
+                @if($this->activeTab !== 'documentExpiry')
+                    <div class="col __col-{{$this->activeTab == 'UnderVerificationBySelf' || $this->activeTab == 'walletBalance' ? 2:3}}">
+                        <div class="form-group">
+                            <label class="custom__label" for="fromDate">From </label>
+                            <input wire:model.live="selectedFromDate" type="date" class="custom__input__field rounded-0 form-control form-control-sm" id="fromDate" placeholder="Enter from date">
+                        </div>
+                    </div>
+                    <div class="col __col-{{$this->activeTab == 'UnderVerificationBySelf' || $this->activeTab == 'walletBalance' ? 2:3}}">
+                        <div class="form-group">
+                            <label class="custom__label" for="toDate">To</label>
+                            <input wire:model.live="selectedToDate" type="date" class="custom__input__field rounded-0 form-control form-control-sm" id="toDate" placeholder="Enter to date">
+                        </div>
+                    </div>
+                    @endif
+                    @if($this->activeTab !== 'documentExpiry')
+
+                    <div class="col -{{$this->activeTab == 'UnderVerificationBySelf' || $this->activeTab == 'walletBalance' ? 2:3}}">
+                        <div class="form-group">
+                            <label class="custom__label">Select</label>
+                            <select wire:model.live.debounce.150ms="selectedDate" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="exampleSelectRounded0">
+                                <option selected value="all">All</option>
+                                <option value="today">Today</option>
+                                <option value="yesterday">Yesterday</option>
+                                <option value="thisWeek">This Week</option>
+                                <option value="thisMonth">This Month</option>
+                            </select>
+                        </div>
+                    </div>
+                    @endif
+                    @if($this->activeTab == 'documentExpiry')
+                    <div class="col">
+                        <div class="form-group">
+                          <label class="custom__label">Select</label>
+                            <select wire:model.live.debounce.150ms="selectDocumentExpiry" wire:model.live="selectDocumentExpiry" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="selectDocumentExpiry">
+                                <option selected value="dl">DL</option>
+                                <option value="rcno">RC NO.</option>
+                                <option value="vehicleInsuarance">Vehicle Insuarance</option>
+                                <option value="vehiclePolution">Vehicle Polution</option>
+                                <option value="vehicleFitness">Vehicle Fitness</option>
+                            </select>
+                        </div>
+                    </div>
+                    @endif
+                    @if($this->activeTab == 'UnderVerificationBySelf')
+                    <div class="col __col-3">
+                        <div class="form-group">
+                            <label class="custom__label">Driver By Status</label>
+                            <select wire:model.live.debounce.150ms="driverVerificationStatus" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                                <option selected value="">Select Verification status</option>
+                                <option value="UnderVerification">Under Verification(All)</option>
+                                <option value="UnderVerificationBySelf">Under Verification(By Self)</option>
+                                <option value="UnderVerificationByPartner">Under Verification(By Partner)</option>
+
+                            </select>
+                        </div>
+                    </div>
+                    @endif
+
+                    @if($this->activeTab == 'walletBalance')
+                    <div class="col __col-3">
+                        <div class="form-group">
+                            <label class="custom__label">Wallet Balance</label>
+                            <select wire:model.live.debounce.150ms="walletBalanceFilter" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                                <option value="positiveBalance">Positive Balance</option>
+                                <option value="zeroBalance">Zero Balance</option>
+                                <option value="negativeBalance">Negative Balance</option>
+                            </select>
+                        </div>
+                    </div>
+                    @endif
+
+                    <div class="col __col-3">
+                        <div class="form-group">
+                            <label class="custom__label" for="toDate">Search</label>
+                            <input type="search" wire:model.live.debounce.150ms="dexpnameSearch" class="custom__input__field form-control rounded-0 form-control-sm float-right" placeholder="Search">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row d-none card-header">
+                <div class="col-3">
+                    <div class="form-group">
+                        <label class="custom__label">Wallet Balance</label>
+                        <select wire:model.live.debounce.150ms="walletBalanceFilter" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                            <option value="positiveBalance">Positive Balance</option>
+                            <option value="zeroBalance">Zero Balance</option>
+                            <option value="negativeBalance">Negative Balance</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label class="custom__label">Wallet Balance</label>
+                        <select wire:model.live.debounce.150ms="walletBalanceFilter" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                            <option value="positiveBalance">Positive Balance</option>
+                            <option value="zeroBalance">Zero Balance</option>
+                            <option value="negativeBalance">Negative Balance</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label class="custom__label">Wallet Balance</label>
+                        <select wire:model.live.debounce.150ms="walletBalanceFilter" class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                            <option value="positiveBalance">Positive Balance</option>
+                            <option value="zeroBalance">Zero Balance</option>
+                            <option value="negativeBalance">Negative Balance</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label class="custom__label">Wallet Balance</label>
+                        <select class="custom__input__field custom-select rounded-0 form-control form-control-sm" id="SelectUnderVerification">
+                            <option value="positiveBalance">Positive Balance</option>
+                            <option value="zeroBalance">Zero Balance</option>
+                            <option value="negativeBalance">Negative Balance</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <!-- <table class="table table-bordered table-sm table-responsive-sm mt-2"> -->
+            <div wire:loading.remove wire:target="filterCondition" class="card-body p-2 overflow-auto">
+                <table class="table custom__table table-bordered table-sm ">
+                    <tr>
+                        <th>Sr.</th>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Mobile</th>
+                        <th>Wallet</th>
+                        @if($this->activeTab !== 'documentExpiry')
+                        <th>Created At</th>
+                        @endif
+                        @if($this->activeTab !== 'documentExpiry')
+                        <th>Created By</th>
+                        @endif
+                        <th>Expiry</th>
+                        <!-- <th>Remark Details</th> -->
+                        <th>City</th>
+                        <th> State</th>
+                        <!-- <th>DL No.</th> -->
+                        <th>Action</th>
+                    </tr>
+                    @php
+                    $srno = 1
+                    @endphp
+                    @foreach ($drivers_data as $list)
+                    <tr>
+                        <td>{{ $srno }}</td>
+                        <td>{{ $list->driver_id }}</td>
+                        <td>{{ $list->driver_name.' '.$list->driver_last_name }}</td>
+                        <td>{{ $list->driver_mobile }}</td>
+                        <td>{{ $list->vehicle_id }}</td>
+                        @if($this->activeTab !== 'documentExpiry')
+                        <td>{{ $list->created_at }}</td>
+                        @endif
+                        @if($this->activeTab !== 'documentExpiry')
+                        <td>
+                            @if($list->driver_created_by=='0')
+                            Self
+                            @else
+                            Partner
+                            @endif
+                        </td>
+                        @endif
+                        
+                        <td>
+                            {{ $list->driver_details_dl_exp_date }}
+                        </td>
+
+                        <td>{{ $list->city_name }}</td>
+                        <td>{{ $list->state_name }}</td>
+                        <td class="action__btn lbtn-group">
+                            <button wire:click="edit({{ $list->driver_id }})" class="btn-primary"><i class="fa fa-edit fa-sm"></i></button>
+                            <button  wire:navigate href="#" class="btn-success"><i class="fa fa-eye fa-sm"></i></button>
+                            <button wire:confirm="Are you sure you want to delete this post?" wire:click="delete({{ $list->driver_id }})" class="btn-danger"><i class="fa fa-trash fa-sm"></i></button>
+                        </td>
+                    </tr>
+                    @php
+                    $srno++
+                    @endphp
+                    @endforeach
+
+                </table>
+                <!-- <span wire:loadingf wire:target="filterCondition">Loading...</span> -->
+                <div class="custom__pagination mt-2 text-right pt-1 card-footer__ clearfix">
+          {!! $drivers_data->links() !!}
+
+                    </div>
+            </div>
+        </div>
+
+        <div class="container h-100 w-100">
+            <div class="row w-100 h-100 align-items-center justify-content-center" wire:loading wire:target="selectedDate,driverVerificationStatus,filterCondition" wire:key="selectedDate,Onduty,Offduty">
+                <div class="col">
+                    <div class="loader">
+                        <div class="loader-inner">
+                            <div class="loading one"></div>
+                        </div>
+                        <div class="loader-inner">
+                            <div class="loading two"></div>
+                        </div>
+                        <div class="loader-inner">
+                            <div class="loading three"></div>
+                        </div>
+                        <div class="loader-inner">
+                            <div class="loading four"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+</div>
