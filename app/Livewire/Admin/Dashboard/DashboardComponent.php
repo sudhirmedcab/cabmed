60b47ae610c9f59fcc35b868caf093cff649b8d3
Saurabh <saurabhawasthi618@gmail.com>
2024-03-21 19:06:42 +0530

    added doughnut chart in dashboard component

--- app/Livewire/Admin/Dashboard/DashboardComponent.php
+++ app/Livewire/Admin/Dashboard/DashboardComponent.php
@@ -8,6 +8,15 @@ class DashboardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.dashboard.dashboard-component');
+        $data1 = [
+            'labels' => ['New Consumer', 'Active Consumer'],
+            'data' => [25, 30],
+        ];
+        $data2 = [
+            'labels' => ['New Consumer', 'Active Consumer'],
+            'data' => [25, 30],
+        ];
+
+        return view('livewire.admin.dashboard.dashboard-component', compact('data1', 'data2'));
     }
 }
--- resources/views/livewire/admin/dashboard/dashboard-component.blade.php
+++ resources/views/livewire/admin/dashboard/dashboard-component.blade.php
@@ -9,9 +9,9 @@
 </style>
 <div class="content">
     <!--  -->
-    <div class="row">
+    <div class="row custom__dashboard__data">
         <div class="col-12 col-sm-6 col-md-4">
-            <div class="container-fluid custom__dashboard__data">
+            <div class="container-fluid">
                 <div class="card mt-2 p-2">
                     <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
                         <h2 class="">Booking</h2>
@@ -40,7 +40,7 @@
             </div>
         </div>
         <div class="col-12 col-sm-6 col-md-4">
-            <div class="container-fluid custom__dashboard__data">
+            <div class="container-fluid">
                 <div class="card mt-2 p-2">
                     <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
                         <h2 class="">Hospital</h2>
@@ -65,10 +65,26 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
         <div class="col-12 col-sm-6 col-md-4">
-            <div class="container-fluid custom__dashboard__data">
+            <div class="container-fluid">
+                <div class="card mt-2 p-2">
+                    <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
+                        <h2 class="">Hospital</h2>
+                        <div class="ml-auto">
+                            <input type="date" class="custom__input__field rounded bg-light form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="p-4" style="width: 80%; margin: auto;">
+                        <canvas id="doughnutChart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row custom__dashboard__data">
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="container-fluid">
                 <div class="card mt-2 p-2">
                     <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
                         <h2 class="">Vehicle</h2>
@@ -94,7 +110,7 @@
             </div>
         </div>
         <div class="col-12 col-sm-6 col-md-4">
-            <div class="container-fluid custom__dashboard__data">
+            <div class="container-fluid">
                 <div class="card mt-2 p-2">
                     <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
                         <h2 class="">Driver</h2>
@@ -119,6 +135,63 @@
                 </div>
             </div>
         </div>
-
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="container-fluid">
+                <div class="card mt-2 p-2">
+                    <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
+                        <h2 class="">Hospital</h2>
+                        <div class="ml-auto">
+                            <input type="date" class="custom__input__field rounded bg-light form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="p-4" style="width: 80%; margin: auto;">
+                        <canvas id="doughnutChart2"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
+<!-- chart.js -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var ctx = document.getElementById('doughnutChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: @json($data1['labels']),
+            datasets: [{
+                data: @json($data1['data']),
+                backgroundColor: [
+                    'rgba(23,156,136)',
+                    'rgba(195,213,214)'
+                ],
+                borderColor: [
+                    'rgba(23,156,136)',
+                    'rgba(195,213,214)'
+                ],
+                borderWidth: 1
+            }]
+        },
+    });
+    var ctx = document.getElementById('doughnutChart2').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: @json($data2['labels']),
+            datasets: [{
+                data: @json($data2['data']),
+                backgroundColor: [
+                    'rgba(23,156,136)',
+                    'rgba(195,213,214)'
+                ],
+                borderColor: [
+                    'rgba(23,156,136)',
+                    'rgba(195,213,214)'
+                ],
+                borderWidth: 1
+            }]
+        },
+    });
+</script>
