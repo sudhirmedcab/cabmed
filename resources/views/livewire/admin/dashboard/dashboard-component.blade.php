082fefa6c4efcb9006d82ac59e6cbb8f9468d154
Uttam Shrestha <shresthauttam66@gmail.com>
2024-03-21 18:51:32 +0530

    dashboard-component ui

--- resources/views/livewire/admin/dashboard/dashboard-component.blade.php
+++ resources/views/livewire/admin/dashboard/dashboard-component.blade.php
@@ -1,7 +1,124 @@
-<div class="content">
-    <div class="container-fluid">
-        <div class="card mt-2">
+<style>
+    .custom__dashboard__data h2 {
+        font-size: 16px;
+    }
 
+    .custom__dashboard__data p {
+        font-size: 12px;
+    }
+</style>
+<div class="content">
+    <!--  -->
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="container-fluid custom__dashboard__data">
+                <div class="card mt-2 p-2">
+                    <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
+                        <h2 class="">Booking</h2>
+                        <div class="ml-auto">
+                            <input type="date" class="custom__input__field rounded bg-light form-control form-control-sm">
+                        </div>
+                        <div class="ml-2 bg-light px-2 rounded border text-center">
+                            <i class="fas fa-ellipsis-h text-secondary"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Enquiry Booking </p>
+                            <p class="m-0">00</p>
+                        </div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Enquiry Booking </p>
+                            <p class="m-0">00</p>
+                        </div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Enquiry Booking </p>
+                            <p class="m-0">00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="container-fluid custom__dashboard__data">
+                <div class="card mt-2 p-2">
+                    <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
+                        <h2 class="">Hospital</h2>
+                        <div class="ml-auto">
+                            <input type="date" class="custom__input__field rounded bg-light form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Hospital </p>
+                            <p class="m-0">00</p>
+                        </div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Hospital </p>
+                            <p class="m-0">00</p>
+                        </div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Hospital </p>
+                            <p class="m-0">00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="container-fluid custom__dashboard__data">
+                <div class="card mt-2 p-2">
+                    <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
+                        <h2 class="">Vehicle</h2>
+                        <div class="ml-auto">
+                            <input type="date" class="custom__input__field rounded bg-light form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Vehicle</p>
+                            <p class="m-0">00</p>
+                        </div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Vehicle</p>
+                            <p class="m-0">00</p>
+                        </div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Vehicle</p>
+                            <p class="m-0">00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="container-fluid custom__dashboard__data">
+                <div class="card mt-2 p-2">
+                    <div class="d-flex mb-2 border-bottom pb-2 align-items-center">
+                        <h2 class="">Driver</h2>
+                        <div class="ml-auto">
+                            <input type="date" class="custom__input__field rounded bg-light form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Driver </p>
+                            <p class="m-0">00</p>
+                        </div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Driver </p>
+                            <p class="m-0">00</p>
+                        </div>
+                        <div class="d-flex justify-content-between border  py-1 px-2 rounded mb-1">
+                            <p class="m-0">Total No of Driver </p>
+                            <p class="m-0">00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
