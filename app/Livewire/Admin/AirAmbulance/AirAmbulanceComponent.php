fd1b523617fa0bed5f7ee281e7c31b0b96952e85
Uttam Shrestha <shresthauttam66@gmail.com>
2024-04-08 19:05:06 +0530

    air ambulance component

---
+++ app/Livewire/Admin/AirAmbulance/AirAmbulanceComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\AirAmbulance;
+
+use Livewire\Component;
+
+class AirAmbulanceComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.air-ambulance.air-ambulance-component');
+    }
+}
---
+++ resources/views/livewire/admin/air-ambulance/air-ambulance-component.blade.php
@@ -0,0 +1,117 @@
+<style>
+    .custom__table td {
+        text-wrap: wrap !important;
+    }
+</style>
+
+<div class="content">
+    <div class="container-fluid">
+        <div class="custom__driver__detail__section card px-3 custom__filter__responsive mt-2">
+            <div class="row p-0 flex-wrap">
+                <div class="col-12 col-md-6 px-0">
+                    <div class="pl-3 py-2">
+                        <table class="table m-0 custom__table custom__wrap__table table-bordered">
+                            <tr>
+                                <td>Booking Id</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Pick Up</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Drop Location</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>DateTime</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Amount</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Type of Air Ambulance</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Duration</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Patient Name</td>
+                                <td></td>
+                            </tr>
+
+                        </table>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 px-0">
+                    <div class="pl-3 py-2">
+                        <table class="table m-0 custom__table custom__wrap__table table-bordered">
+                            <tr>
+                                <td>Aadhaar No</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Pan No</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Mobile No</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>GST</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Aadhaar Front Image</td>
+                                <td>View Image</td>
+                            </tr>
+                            <tr>
+                                <td>Aadhaar Back Image</td>
+                                <td>View Image</td>
+                            </tr>
+                            <tr>
+                                <td>Pan Image</td>
+                                <td>View Image</td>
+                            </tr>
+                            <tr>
+                                <td>Covid Report</td>
+                                <td>View Image</td>
+                            </tr>
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="custom__driver__detail__section card px-3">
+        <table class="table m-0 table-sm custom__table table-bordered mt-3 mb-2">
+            <tr>
+                <th>Sr.No.</th>
+                <th>Name</th>
+                <th>Aadhaar Front</th>
+                <th>Aadhaar Back</th>
+                <th>Pan Image</th>
+                <th>Covid Report</th>
+                <th>Pan Number</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+</div>
+</div>
