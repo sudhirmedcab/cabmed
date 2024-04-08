a14762802bd766446f12e4d63c8bf4f9e43eeadf
Sukhiram <bonusahu1999@gmail.com>
2024-04-08 11:04:05 +0530

    modify the consumer booking details to remody the ambulance booking list and pathology booking list with his filter and backends functionality

--- app/Livewire/Admin/Consumer/ConsumerBookingDetailsComponent.php
+++ app/Livewire/Admin/Consumer/ConsumerBookingDetailsComponent.php
@@ -5,15 +5,16 @@ namespace App\Livewire\Admin\Consumer;
 use Illuminate\Support\Facades\DB; 
 use Livewire\Component;
 use Carbon\Carbon;
-use Livewire\WithoutUrlPagination;
+// use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
 
 class ConsumerBookingDetailsComponent extends Component
 {
+    use WithPagination;
 
     public $consumerId,$activeTab,$booking_status,$selectedDate,$filterConditionl, 
     $selectedFromDate,$selectedToDate, $fromDate=null, 
-    $toDate=null,$fromdate, $todate,$check_for;
+    $toDate=null,$fromdate, $todate,$check_for,$selectBookingStatus,$healthCardVerificationStatus,$selectPathologyBookingStatus;
 
     protected $paginationTheme = 'bootstrap';
     // 
@@ -24,45 +25,31 @@ class ConsumerBookingDetailsComponent extends Component
 
     public function resetFilters(){
         $this->selectedDate=null;
+        // $this->resetPage();
+        $this->search = '';
     }
 
     public function filterCondition($value){
 
         $this->resetFilters();
-        if($value=='driver_details'){            
-            $this->activeTab=$value;
-        }
     
         if($value=='Enquiry'){
             $this->booking_status='0';
             $this->activeTab=$value;
         }
-        if($value=='New'){
-            $this->booking_status = '1';
-            $this->activeTab=$value;
-        }
-    
-        if($value=='Ongoing'){
-            $this->booking_status='2';
-            $this->activeTab=$value;
-        }
-        if($value=='Invoice'){
-            $this->booking_status = '3';
-            $this->activeTab=$value;
-        }
-    
-        if($value=='Complete'){
-            $this->booking_status='4';
-            $this->activeTab=$value;
-        }
-        if($value=='Cancel'){
-            $this->booking_status = '5';
-            $this->activeTab=$value;
-        }
+        
         if($value=='Transaction'){
             $this->activeTab=$value;
         }
         if($value=='consumer_transaction'){
+            $this->activeTab=$value;
+        }
+
+        if($value=='pathology_booking'){
+            $this->activeTab=$value;
+        }
+
+        if($value=='HealthCard'){
             $this->activeTab=$value;
         }
   
@@ -70,56 +57,106 @@ class ConsumerBookingDetailsComponent extends Component
     public function render()
     {        
         $booking_status = $this->booking_status ? $this->booking_status : null;
+        $selectBookingStatus = $this->selectBookingStatus ? $this->selectBookingStatus : "New";
+        $selectPathologyBookingStatus = $this->selectPathologyBookingStatus ? $this->selectPathologyBookingStatus : "New";
+        $healthCardVerificationStatus = $this->healthCardVerificationStatus ? $this->healthCardVerificationStatus : null;
+
         $consumerId = $this->consumerId;
 
         try {
+
          $decryptconsumerId = decrypt($consumerId);
          } catch (DecryptException $e) {
              abort(403, 'Unauthorized action.');
          }
-
+        
+         if(!$booking_status == 'HealthCard' || ! $booking_status == 'pathology_booking' || ! $booking_status == 'transaction'){
+            $fromDate = $this->selectedFromDate ? Carbon::createFromFormat('Y-m-d', $this->selectedFromDate)->startOfDay() : null;
+            $toDate = $this->selectedToDate ? Carbon::createFromFormat('Y-m-d', $this->selectedToDate)->endOfDay() : null;
+    
+            if($this->selectedFromDate && $this->selectedToDate){
+                    $this->selectedDate = null;     
+                }
                 
-        $fromDate = $this->selectedFromDate ? Carbon::createFromFormat('Y-m-d', $this->selectedFromDate)->startOfDay() : null;
-        $toDate = $this->selectedToDate ? Carbon::createFromFormat('Y-m-d', $this->selectedToDate)->endOfDay() : null;
+                if($this->selectedDate == 'custom'){
+                    $this->selectedFromDate;
+                    $this->selectedToDate;
+                }else{
+                    $this->selectedFromDate ='';
+                    $this->selectedToDate =''; 
+                }
+    
+                switch ($this->selectedDate) {
+                    case 'all':
+                        $fromDate = null;
+                        $toDate = null;
+                        break;
+                    case 'today':
+                        $fromDate = Carbon::today();
+                        $toDate = Carbon::today()->endOfDay();
+                        break;
+                    case 'yesterday':
+                        $fromDate = Carbon::yesterday();
+                        $toDate = Carbon::yesterday()->endOfDay();
+                        break;
+                    case 'thisWeek':
+                        $fromDate = Carbon::now()->subDays(7)->startOfDay();
+                        $toDate = Carbon::now();
+                        break;
+                    case 'thisMonth':
+                        $fromDate = Carbon::now()->startOfMonth();
+                        $toDate = Carbon::now()->endOfMonth();
+                        break;
+                    default:
+                        $fromDate = $fromDate;
+                        $toDate = $toDate;
+                        break;
+                }
+         }else{
+
+       $fromDate = $this->selectedFromDate ? strtotime($this->selectedFromDate) : null;
+        $toDate = $this->selectedToDate ?  strtotime($this->selectedToDate) : null;
 
         if($this->selectedFromDate && $this->selectedToDate){
-                $this->selectedDate = null;     
-            }
-            
-            if($this->selectedDate == 'custom'){
-                $this->selectedFromDate;
-                $this->selectedToDate;
-            }else{
-                $this->selectedFromDate ='';
-                $this->selectedToDate =''; 
-            }
+            $this->selectedDate = null;     
+        }
+        
+        if($this->selectedDate == 'custom'){
+            $this->selectedFromDate;
+            $this->selectedToDate;
+        }else{
+            $this->selectedFromDate ='';
+            $this->selectedToDate =''; 
+        }
 
-            switch ($this->selectedDate) {
-                case 'all':
-                    $fromDate = null;
-                    $toDate = null;
-                    break;
-                case 'today':
-                    $fromDate = Carbon::today();
-                    $toDate = Carbon::today()->endOfDay();
-                    break;
-                case 'yesterday':
-                    $fromDate = Carbon::yesterday();
-                    $toDate = Carbon::yesterday()->endOfDay();
-                    break;
-                case 'thisWeek':
-                    $fromDate = Carbon::now()->subDays(7)->startOfDay();
-                    $toDate = Carbon::now();
-                    break;
-                case 'thisMonth':
-                    $fromDate = Carbon::now()->startOfMonth();
-                    $toDate = Carbon::now()->endOfMonth();
-                    break;
-                default:
-                    $fromDate = $fromDate;
-                    $toDate = $toDate;
-                    break;
-            }
+        switch ($this->selectedDate) {
+            case 'all':
+                $fromDate = null;
+                $toDate = null;
+                break;
+            case 'today':
+                $fromDate = strtotime(date('Y-m-d'));
+                $toDate = strtotime(date('Y-m-d').' 23:59:59');
+                break;
+            case 'yesterday':
+                $fromDate = strtotime('-1 day', strtotime(date('Y-m-d')));
+                $toDate = strtotime('-1 day', strtotime(date('Y-m-d').' 23:59:59'));
+                break;
+            case 'thisWeek':
+                $fromDate = strtotime('-7 days', strtotime(date('Y-m-d')));
+                $toDate = strtotime(date('Y-m-d').' 23:59:59');
+                break;
+            case 'thisMonth':
+                $fromDate = strtotime(date('Y-m-01'));
+                $toDate = strtotime(date('Y-m-t').' 23:59:59');
+                break;
+            default:
+                $fromDate = $fromDate;
+                $toDate = $toDate;
+                break;
+        }
+
+         }
 
             if($booking_status == 'transaction'){
 
@@ -130,7 +167,7 @@ class ConsumerBookingDetailsComponent extends Component
                         ->orWhere('consumer.consumer_mobile_no', 'like', '%' . $this->search . '%');
                 })  
                 ->when($fromDate && $toDate, function ($query) use ($fromDate, $toDate) {
-                    return $query->whereBetween('booking_payments.created_at', [$fromDate, $toDate]);
+                    return $query->whereBetween('booking_payments.booking_transaction_time', [$fromDate, $toDate]);
                 }) 
                 ->where('booking_payments.consumer_id','=',$decryptconsumerId)
                 ->orderBy('consumer.consumer_id','desc')
@@ -146,6 +183,57 @@ class ConsumerBookingDetailsComponent extends Component
                        return view('livewire.admin.consumer.consumer-booking-details-component',['isCustom'=>false],compact('transactionDetails','consumerDetails'));
                    }            
             }
+
+
+
+
+            if($booking_status=='pathology_booking'){
+        
+            $pathologybookingDetails = DB::table('customer_lab_order')
+            ->leftJoin('consumer', 'consumer.consumer_id', '=', 'customer_lab_order.clo_user_id')
+            ->leftJoin('city', 'city.city_id', '=', 'customer_lab_order.clo_address_city_id')
+            ->leftJoin('state', 'state.state_id', '=', 'city.city_state')
+            ->where(function ($query) {
+                $query->where('customer_lab_order.clo_customer_name', 'like', '%' . $this->search . '%')
+                    ->orWhere('customer_lab_order.clo_contact_no', 'like', '%' . $this->search . '%');
+            })        
+            ->where('customer_lab_order.clo_user_id','=',$decryptconsumerId)
+            ->when($fromDate && $toDate, function ($query) use ($fromDate, $toDate) {
+                return $query->whereBetween('customer_lab_order.clo_order_time', [$fromDate, $toDate]);
+            }) 
+            ->when($selectPathologyBookingStatus != 'All', function ($query) use ($selectPathologyBookingStatus) {
+                return $query->when($selectPathologyBookingStatus == 'Cancel', function ($query) {
+                    return $query->where('customer_lab_order.clo_status',3);
+                })
+                ->when($selectPathologyBookingStatus == 'New', function ($query) {
+                    return $query->where('customer_lab_order.clo_status',1);
+                })
+            
+                ->when($selectPathologyBookingStatus == 'Ongoing', function ($query) {
+                    return $query->where('customer_lab_order.clo_status',2);
+                })
+                ->when($selectPathologyBookingStatus == 'Complete', function ($query) {
+                    return $query->where('customer_lab_order.clo_status',4);
+                });
+            
+            })
+            ->orderBy('customer_lab_order.customer_lab_order_id','desc')
+            ->paginate(10);
+
+            $consumerDetails = DB::table('consumer')
+            ->where('consumer_id', '=', $decryptconsumerId)
+            ->first();
+
+            if($this->check_for == 'custom'){
+
+                return view('livewire.admin.consumer.consumer-booking-details-component',['isCustom'=>true],compact('pathologybookingDetails','consumerDetails'));
+                }else {
+                    return view('livewire.admin.consumer.consumer-booking-details-component',['isCustom'=>false],compact('pathologybookingDetails','consumerDetails'));
+                }  
+
+            }
+
+
 
             if($booking_status == 'consumer_transaction'){
 
@@ -167,6 +255,60 @@ class ConsumerBookingDetailsComponent extends Component
                    }            
             }
 
+
+
+        if($booking_status == 'HealthCard'){
+
+            $healthcardData = DB::table('health_card_subscription')
+            ->leftJoin('health_card_plan', 'health_card_plan.health_card_plan_id', '=', 'health_card_subscription.health_card_subscription_plan_id')
+            ->leftJoin('consumer', 'consumer.consumer_id', '=', 'health_card_subscription.health_card_subscription_cid')
+            ->leftJoin('health_card_remark', 'health_card_remark.helth_subs_id', '=', 'health_card_subscription.health_card_subscription_id')
+            ->leftJoin('user_address', 'user_address.ua_user_id', '=', 'health_card_subscription.health_card_subscription_address_id')
+            ->when($fromDate && $toDate, function ($query) use ($fromDate, $toDate) {
+                return $query->whereBetween('health_card_subscription.health_card_subscription_added_time_unx', [$fromDate, $toDate]);
+            }) 
+            ->where(function ($query) {
+                $query->where('consumer.consumer_name', 'like', '%' . $this->search . '%')
+                    ->orWhere('consumer.consumer_mobile_no', 'like', '%' . $this->search . '%');
+            })
+            ->where('health_card_subscription.health_card_subscription_cid','=',$decryptconsumerId)
+            ->when($healthCardVerificationStatus == 'AllVerification', function ($query) use ($healthCardVerificationStatus) {
+                return $query
+                    ->whereIn('health_card_subscription.health_card_subscription_status', [1,0,2,3]);
+            })
+            ->when($healthCardVerificationStatus == 'NewVerification', function ($query) use ($healthCardVerificationStatus) {
+                return $query
+                    ->where('health_card_subscription.health_card_subscription_status', 0);
+            })
+            ->when($healthCardVerificationStatus == 'ActiveVerification', function ($query) use ($healthCardVerificationStatus) {
+                return $query
+                    ->where('health_card_subscription.health_card_subscription_status', 2);
+            })
+            ->when($healthCardVerificationStatus == 'AppliedVerification', function ($query) use ($healthCardVerificationStatus) {
+                return $query
+                    ->where('health_card_subscription.health_card_subscription_status', 1);
+            })
+            ->when($healthCardVerificationStatus == 'InactiveVerification', function ($query) use ($healthCardVerificationStatus) {
+                return $query
+                    ->where('health_card_subscription.health_card_subscription_status', 3);
+            })
+            ->orderByDesc('health_card_subscription.health_card_subscription_id')
+            ->paginate(10);
+
+            $consumerDetails = DB::table('consumer')
+            ->where('consumer_id', '=', $decryptconsumerId)
+            ->first();
+
+       if($this->check_for == 'custom'){
+        return view('livewire.admin.consumer.consumer-booking-details-component',['isCustom'=>true],compact('healthcardData','consumerDetails'));
+        }else {
+           return view('livewire.admin.consumer.consumer-booking-details-component',['isCustom'=>false],compact('healthcardData','consumerDetails'));
+       }  
+
+        }
+
+        
+
         $bookingDetails = DB::table('booking_view')
         ->leftJoin('consumer', 'booking_view.booking_by_cid', '=', 'consumer.consumer_id')
         ->where(function ($query) {
@@ -174,11 +316,30 @@ class ConsumerBookingDetailsComponent extends Component
                         ->orWhere('consumer.consumer_mobile_no', 'like', '%' . $this->search . '%');
                 })       
          ->where('booking_view.booking_by_cid','=',$decryptconsumerId)
+          ->whereNot('booking_view.booking_status',7)
          ->when($fromDate && $toDate, function ($query) use ($fromDate, $toDate) {
             return $query->whereBetween('booking_view.created_at', [$fromDate, $toDate]);
         }) 
-        ->when($booking_status !== '0', function ($query) use ($booking_status) {
-            return $query->where('booking_view.booking_status', $this->booking_status);
+        ->when($selectBookingStatus != 'All', function ($query) use ($selectBookingStatus) {
+            return $query->when($selectBookingStatus == 'Cancel', function ($query) {
+                return $query->where('booking_view.booking_status',5);
+            })
+            ->when($selectBookingStatus == 'New', function ($query) {
+                return $query->where('booking_view.booking_status',1);
+            })
+            ->when($selectBookingStatus == 'Enquiry', function ($query) {
+                return $query->where('booking_view.booking_status',0);
+            })
+            ->when($selectBookingStatus == 'Ongoing', function ($query) {
+                return $query->where('booking_view.booking_status',2);
+            })
+            ->when($selectBookingStatus == 'Invoice', function ($query) {
+                return $query->where('booking_view.booking_status',3);
+            })
+            ->when($selectBookingStatus == 'Complete', function ($query) {
+                return $query->where('booking_view.booking_status',4);
+            });
+         
         })
         ->select(
             'booking_view.booking_id',
